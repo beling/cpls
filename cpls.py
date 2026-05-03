19bf2d93c0cf5ffe1213e49d87b781d6fe171fa8
Piotr Beling <piotr.beling@wmii.uni.lodz.pl>
2026-05-03 21:19:48 +0200

    refactoring

--- cpls.py
+++ cpls.py
@@ -80,16 +80,15 @@ def dst_file(src_file: Path) -> Path:
         src_file = src_file.with_suffix('.mp3')
     return Path(src_file.name)
 
-total_files = 0
 dsts = defaultdict(list)    # maps destination file name to source file name(s)
 with open(playlist_filename) as f:
     for src_file in f:
         if src_file.startswith("#"): continue
         src_file = playlist_filename.parent / Path(src_file.strip())
         dsts[dst_file(src_file)].append(src_file)
-        total_files += 1
 
-copied = set()
+to_del = set(f.name for f in dst_dir.iterdir() if f.is_file())
+dst_to_src = {}
 skipped = 0
 converted = 0
 while dsts:
@@ -97,30 +96,35 @@ while dsts:
     number = 1
     for src_file in src_files:
         dst_file = dst_file_candidate
-        while dst_file in dsts or dst_file.name in copied:
+        while dst_file in dsts or dst_file in dst_to_src:
             dst_file = dst_file.with_stem(dst_file_candidate.stem + str(number))
             number += 1
-        copied.add(dst_file.name)
-        print(f"[{len(copied)}/{total_files}] {src_file.name}",
-            f" -> {dst_file.name}" if src_file.name != dst_file.name else '', sep='')
-        dst_file = dst_dir / dst_file
-        if not args.replace and dst_file.exists():
-            print("  destination file exists, skipped (run with -r flag to overwrite)")
-            skipped += 1
-            continue
-        if src_file.suffix[1:].lower() in supported_formats: # copy:
-            if real_run: copyfile(src_file, dst_file)
-        else:   # convert to mp3:
-            if real_run: subprocess.run(["ffmpeg",
-                "-hide_banner", "-loglevel", "error",
-                "-i", str(src_file), "-codec:a", "libmp3lame", "-qscale:a", "2",
-                "-map_metadata", "0:s:a:0", "-id3v2_version", "3", "-write_id3v1", "1",
-                str(dst_file)])
-            converted += 1
-print(f'{len(copied)} processed, {converted} transcoded, {skipped} skipped')
+        dst_to_src[dst_file] = src_file
+        to_del.discard(dst_file.name)
+
+total_files = len(dst_to_src)
+skipped = 0
+converted = 0
+for idx, (dst_file, src_file) in enumerate(dst_to_src.items(), start=1):
+    print(f"[{idx}/{total_files}] {src_file.name}",
+        f" -> {dst_file.name}" if src_file.name != dst_file.name else '', sep='')
+    dst_file = dst_dir / dst_file
+    if not args.replace and dst_file.exists():
+        print("  destination file exists, skipped (run with -r flag to overwrite)")
+        skipped += 1
+        continue
+    if src_file.suffix[1:].lower() in supported_formats: # copy:
+        if real_run: copyfile(src_file, dst_file)
+    else:   # convert to mp3:
+        if real_run: subprocess.run(["ffmpeg",
+            "-hide_banner", "-loglevel", "error",
+            "-i", str(src_file), "-codec:a", "libmp3lame", "-qscale:a", "2",
+            "-map_metadata", "0:s:a:0", "-id3v2_version", "3", "-write_id3v1", "1",
+            str(dst_file)])
+        converted += 1
+print(f'{len(dst_to_src)} processed, {converted} transcoded, {skipped} skipped')
 
 # Suggests deleting extra files from destination:
-to_del = [f.name for f in dst_dir.iterdir() if f.is_file() and f.name not in copied]
 if to_del:
     print(f'The destination directory contains {len(to_del)} extra files:')
     for to_del_file in to_del:
