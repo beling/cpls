e0aaaf9498be9f057945226ea1262c94b53215ac
Piotr Beling <piotr.beling@wmii.uni.lodz.pl>
2026-08-13 19:31:18 +0200

    improved map_metadata

--- cpls.py
+++ cpls.py
@@ -167,7 +167,8 @@ for idx, (dst_file, (src_file, metadata)) in enumerate(reversed(dst_to_src.items
         if real_run: subprocess.run(["ffmpeg",
             "-hide_banner", "-y", "-loglevel", "error",
             "-i", str(src_file), "-codec:a", "libmp3lame", "-qscale:a", "2",
-            "-map_metadata", "0:s:a:0", "-id3v2_version", "3", "-write_id3v1", "1",
+            "-map_metadata", "0:s:a:0" if src_file.suffix.lower() in ('.opus', '.ogg') else "0",
+            "-id3v2_version", "3", "-write_id3v1", "1",
             str(dst_file)])
         converted += 1
 
